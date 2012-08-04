3c939438319117bf312b1c1aa8cb5c5799edfdac
Michael Rog <michael@michaelrog.com>
2012-08-04 13:20:58 -0500

    more project formatting

--- system/expressionengine/third_party/scraper/pi.scraper.php
+++ system/expressionengine/third_party/scraper/pi.scraper.php
@@ -1,67 +1,114 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php
 
-/**
- * ExpressionEngine - by EllisLab
- *
- * @package		ExpressionEngine
- * @author		ExpressionEngine Dev Team
- * @copyright	Copyright (c) 2003 - 2011, EllisLab, Inc.
- * @license		http://expressionengine.com/user_guide/license.html
- * @link		http://expressionengine.com
- * @since		Version 2.0
- * @filesource
- */
- 
-// ------------------------------------------------------------------------
+/*
+=====================================================
 
-/**
- * Scraper Plugin
- *
- * @package		ExpressionEngine
- * @subpackage	Addons
- * @category	Plugin
- * @author		Michael Rog
- * @link		http://rog.ee
- */
+RogEE "Scraper"
+a plugin for ExpressionEngine 2
+by Michael Rog
+
+Contact Michael with questions, feedback, suggestions, bugs, etc.
+>> http://rog.ee/template_omnilogger
+
+=====================================================
+*/
+
+if (!defined('APP_VER') || !defined('BASEPATH')) { exit('No direct script access allowed'); }
+
+// -----------------------------------------
+//	Here goes nothin...
+// -----------------------------------------
+
+require_once PATH_THIRD.'scraper/config.php';
 
 $plugin_info = array(
-	'pi_name'		=> 'Scraper',
-	'pi_version'	=> '0.0',
-	'pi_author'		=> 'Michael Rog',
-	'pi_author_url'	=> 'http://rog.ee',
-	'pi_description'=> 'Scrapes content (optionally filtered by ID) from a page at a remote URL and displays it in your template',
+	'pi_name'		=> ROGEE_SCRAPER_NAME,
+	'pi_version'	=> ROGEE_SCRAPER_VERSION,
+	'pi_author'		=> ROGEE_SCRAPER_AUTHOR,
+	'pi_author_url'	=> ROGEE_SCRAPER_AUTHOR_URL,
+	'pi_description'=> ROGEE_SCRAPER_DESC,
 	'pi_usage'		=> Scraper::usage()
 );
 
 
+/**
+ * ==============================================
+ * Simplehtmldom library, created by S. C. Chen
+ * ==============================================
+ *
+ * @author S. C. Chen
+ * @see http://simplehtmldom.sourceforge.net/
+ *
+ */
+
+require_once PATH_THIRD.'scraper/libraries/simplehtmldom/simple_html_dom.php';
+
+/**
+ * ==============================================
+ * Scraper class, for ExpressionEngine 2
+ * ==============================================
+ *
+ * @package Scraper
+ * @author Michael Rog <michael@michaelrog.com>
+ * @copyright 2012 Michael Rog
+ * @see http://rog.ee/scraper
+ *
+ */
 class Scraper {
+
+	private $EE;
+	private $H;
+	private $S;
 
 	public $return_data;
     
 	/**
-	 * Constructor
-	 */
+	* ==============================================
+	* Constructor
+	* ==============================================
+	*
+	* @access  public
+	* @return  void
+	*
+	*/
 	public function __construct()
 	{
+	
 		$this->EE =& get_instance();
+		
+		// Load the RogEE helpers model
+		$this->EE->load->model('rogee_helpers_model');
+		$this->H = $this->EE->rogee_helpers_model;
+	
 	}
 	
-	// ----------------------------------------------------------------
 	
 	/**
-	 * Plugin Usage
-	 */
+	* ==============================================
+	* usage()
+	* ==============================================
+	*
+	* Provides usage information for display in the control panel
+	*
+	* @return string
+	*
+	*/
 	public static function usage()
 	{
+	
 		ob_start();
 ?>
 
-TODO: Plugin docs go here!
+For more information, see the complete docs:
+http://rog.ee/scraper
+
 <?php
 		$buffer = ob_get_contents();
 		ob_end_clean();
 		return $buffer;
+		
 	}
+
 }
 
 
